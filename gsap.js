ca7a6fc14696eb71fc6ed28067879d3ba33a3040
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-01-20 15:56:10 +0530

    Zoom Out Reveal Content Animation : Added the bg Image in section 1 as a revealed content

---
+++ gsap.js
@@ -0,0 +1,33 @@
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: "top top",
+        end: "+=150%",
+        pin: true,
+        scrub: 4,
+        markers: false
+      }
+    })
+    .to("img", {
+      scale: 2,
+      z: 350,
+      force3D: true,
+      transformOrigin: "center center",
+      ease: "power1.inOut"
+    })
+    .to(
+      ".section.hero",
+      {
+        scale: 1.1,
+        transformOrigin: "center center",
+        ease: "power1.inOut"
+      },
+      "<"
+    );
+});
